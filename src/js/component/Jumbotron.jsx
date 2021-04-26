b5905f7ef171abc5a1d0ff58d8370a606b60eaab
pamelabarrionuevo <pamela@nextchallenge.cl>
2021-04-26 23:38:44 +0000

    componentes navbar y jumbotron

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+export function Jumbotron() {
+	return (
+		<div className="jumbotron container center">
+			<h1 className="display-4">A Warm Welcome!</h1>
+			<p>
+				Wisi forensibus mnesarchum in cum. Per id impetus abhorreant,
+				his no magna definiebas, inani rationibus in quo. Ut vidisse
+				dolores est, ut quis nominavi mel. Ad pri quod apeirian.
+			</p>
+			<a className="btn btn-primary btn-lg" href="#" role="button">
+				Call to action!
+			</a>
+		</div>
+	);
+}
+
+export default Jumbotron;
